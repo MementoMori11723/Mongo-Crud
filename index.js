50fa5500a8e2f2f025a36f5de9f1059253ec9d42
yasasvi <yasasvigumma@gmail.com>
2024-03-20 19:21:12 +0530

    adding packages

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hi");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello User!");
+});
+
+app.listen(5173, () => {
+  console.log("Listening to app on http://localhost:5173");
+});
